44cc393be37dc0597a33a0533d55b8f3d3874acf
Joseph DALY <joseph300905@gmail.com>
2026-02-13 11:32:00 +0100

    basic Board implementation

--- quarto/src/lib.rs
+++ quarto/src/lib.rs
@@ -34,3 +34,19 @@ impl From<u8> for Piece {
         Self(value)
     }
 }
+
+pub struct Board([[Option<Piece>; 4]; 4]);
+
+impl Board {
+    pub fn new() -> Self {
+        Board([[None; 4]; 4])
+    }
+
+    pub fn get_piece(&self, x: usize, y: usize) -> Option<Piece> {
+        self.0[x][y]
+    }
+
+    pub fn set_piece(&mut self, x: usize, y: usize, piece: Option<Piece>) {
+        self.0[x][y] = piece;
+    }
+}
