2cc561238cd03856754bef448fa0d33465bc18cf
Joseph DALY <joseph300905@gmail.com>
2026-02-14 11:25:46 +0100

    core: implement find for Stack

--- core/src/stack.rs
+++ core/src/stack.rs
@@ -13,6 +13,14 @@ impl Stack {
         Stack(arr)
     }
 
+    pub fn find(&self, piece: Piece) -> Option<usize> {
+        self.0
+            .iter()
+            .enumerate()
+            .find(|(_, p)| Some(piece) == **p)
+            .map(|(i, _)| i)
+    }
+
     pub fn pick(&mut self, i: usize) -> Option<Piece> {
         if i > 16 {
             return None;
@@ -33,5 +41,24 @@ impl Stack {
 impl Default for Stack {
     fn default() -> Self {
         Self::new()
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_find() {
+        let mut stack = Stack::new();
+
+        assert_eq!(Some(2), stack.find(Piece(0b0010)));
+        assert_eq!(Some(5), stack.find(Piece(0b0101)));
+        assert_eq!(None, stack.find(Piece(0b11111111)));
+
+        _ = stack.pick(2);
+        assert_eq!(None, stack.find(Piece(0b0010)));
+        assert_eq!(Some(5), stack.find(Piece(0b0101)));
+        assert_eq!(None, stack.find(Piece(0b11111111)));
     }
 }
