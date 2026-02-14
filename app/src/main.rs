53e859c2d96a1a0a089ec9850223c8112f353e34
Joseph DALY <joseph300905@gmail.com>
2026-02-14 12:01:11 +0100

    app: extract main game loop logic to its own function

--- app/src/main.rs
+++ app/src/main.rs
@@ -1,10 +1,45 @@
 use quarto_core::{Board, Game, Stack};
 use quarto_players::Player;
 
+const PLAYER_COUNT: usize = 2;
+
 pub enum Outcome {
     Win(usize),
     Draw,
     Illegal(usize),
+}
+
+pub fn game_iter(
+    game: &mut Game,
+    player_idx: usize,
+    players: &mut [Box<dyn Player>; PLAYER_COUNT],
+) -> Option<Outcome> {
+    let current_player = players.get_mut(player_idx).unwrap();
+    let piece_idx = current_player.give_piece(game);
+
+    if !game.stack.0.iter().any(|p| p.is_some()) {
+        return Some(Outcome::Draw);
+    }
+
+    let piece = match game.stack.pick(piece_idx) {
+        Some(piece) => piece,
+        None => return Some(Outcome::Illegal(player_idx)),
+    };
+
+    let next_idx = (player_idx + 1) % PLAYER_COUNT;
+    let next_player = players.get_mut(next_idx).unwrap();
+    let (x, y) = next_player.play_piece(game, piece);
+
+    if game.board.get_piece(x, y).is_some() {
+        return Some(Outcome::Illegal(next_idx));
+    }
+    game.board.set_piece(x, y, Some(piece));
+
+    if game.board.is_win(x, y) {
+        return Some(Outcome::Win(next_idx));
+    }
+
+    None
 }
 
 pub fn game_loop(players: &mut [Box<dyn Player>; 2]) -> Outcome {
@@ -17,29 +52,8 @@ pub fn game_loop(players: &mut [Box<dyn Player>; 2]) -> Outcome {
 
     loop {
         for i in 0..player_count {
-            let current_player = players.get_mut(i).unwrap();
-            let piece_idx = current_player.give_piece(&game);
-
-            if !game.stack.0.iter().any(|p| p.is_some()) {
-                return Outcome::Draw;
-            }
-
-            let piece = match game.stack.pick(piece_idx) {
-                Some(piece) => piece,
-                None => return Outcome::Illegal(i),
-            };
-
-            let next_idx = (i + 1) % player_count;
-            let next_player = players.get_mut(next_idx).unwrap();
-            let (x, y) = next_player.play_piece(&game, piece);
-
-            if game.board.get_piece(x, y).is_some() {
-                return Outcome::Illegal(next_idx);
-            }
-            game.board.set_piece(x, y, Some(piece));
-
-            if game.board.is_win(x, y) {
-                return Outcome::Win(next_idx);
+            if let Some(outcome) = game_iter(&mut game, i, players) {
+                return outcome;
             }
         }
     }
